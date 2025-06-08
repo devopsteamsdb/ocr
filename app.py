f6ecf0f6a03b09f89db7fec217abf41058cd486d
devopsteamsdb <91658727+devopsteamsdb@users.noreply.github.com>
2025-06-08 14:47:11 +0300

    change UPLOAD_FOLDER to /tmp

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pytesseract
 import os
 
 app = Flask(__name__)
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = '/tmp'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route('/', methods=['GET', 'POST'])
